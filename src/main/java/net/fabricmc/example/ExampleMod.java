84406f25b209f0dfaf893ab7db95a595ef7f4f53
Neil <h66r10@gmail.com>
2021-03-18 01:03:16 +0100

    just a standard biome modification via mixins

---
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -0,0 +1,18 @@
+package net.fabricmc.example;
+
+import kaptainwutax.biomeutils.source.BiomeSource;
+import kaptainwutax.seedutils.mc.Dimension;
+import kaptainwutax.seedutils.mc.MCVersion;
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleMod implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		System.out.println("The mod was loaded");
+	}
+}
