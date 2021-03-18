49a9bae9d8bf420bb43b81cc05fb7540a6d35fe4
Neil <h66r10@gmail.com>
2021-03-18 02:32:39 +0100

    changelog and fix end

---
+++ CHANGELOG.md
@@ -0,0 +1,1 @@
+Added NETHER, OVERWORLD, END
--- src/main/java/com/seedfinding/neil/mixin/End.java
+++ src/main/java/com/seedfinding/neil/mixin/End.java
@@ -10,6 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static com.seedfinding.neil.ModifyBiomes.MC_VERSION;
@@ -19,8 +20,8 @@ public class End {
     @Shadow @Final private Registry<Biome> biomeRegistry;
     public BiomeSource biomeSource;
 
-    @Inject(method = "Lnet/minecraft/world/biome/source/TheEndBiomeSource;<init>(Lnet/minecraft/util/registry/Registry;JLnet/minecraft/world/biome/Biome;Lnet/minecraft/world/biome/Biome;Lnet/minecraft/world/biome/Biome;Lnet/minecraft/world/biome/Biome;Lnet/minecraft/world/biome/Biome;)V", at = @At("RETURN"))
-    private void TheEndBiomeSource(Registry<Biome> biomeRegistry, long seed, Biome centerBiome, Biome highlandsBiome, Biome midlandsBiome, Biome smallIslandsBiome, Biome barrensBiome) {
+    @Inject(method = "<init>(Lnet/minecraft/util/registry/Registry;JLnet/minecraft/world/biome/Biome;Lnet/minecraft/world/biome/Biome;Lnet/minecraft/world/biome/Biome;Lnet/minecraft/world/biome/Biome;Lnet/minecraft/world/biome/Biome;)V", at = @At("RETURN"))
+    private void TheEndBiomeSource(Registry<Biome> biomeRegistry, long seed, Biome centerBiome, Biome highlandsBiome, Biome midlandsBiome, Biome smallIslandsBiome, Biome barrensBiome, CallbackInfo ci) {
         this.biomeSource=BiomeSource.of(Dimension.END,MC_VERSION,seed);
     }
 
